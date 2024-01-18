698dce73fced10b7698419857c19f0d4f48f019f
ldgfelipe <ldgfelipecarrera@gmail.com>
2024-01-18 11:35:14 -0600

    se crea repositorio

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+     Bienvenido a ECOSAE
+    
+    </v-card-title>
+        <v-card-text>
+      Administración de sistema Analogica-Digital
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+         
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
